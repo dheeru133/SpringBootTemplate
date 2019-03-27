5d3cfffd8bed154d4ae8bee773eea40513d79984
Dheeraj Choudhary <DChoudhary@scoular.com>
2019-03-27 07:57:07 -0500

    Spring Boot MVC Implementation

--- src/main/java/com/example/SpringBootTemplateApplication.java
+++ src/main/java/com/example/SpringBootTemplateApplication.java
@@ -1,13 +1,15 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan({ "com.example.controller", "com.example.service", "com.example.config" })
+@ComponentScan({ "com.example.controller", "com.example.config", "com.example.service" })
 public class SpringBootTemplateApplication {
 
 	public static void main(String[] args) {
